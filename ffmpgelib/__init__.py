009ac672cdff8b66a15c2670aac73b4574bb21da
afhernani <afhernani@gmail.com>
2020-09-19 18:00:54 +0100

    add anothers modules graphics and imageblock

--- ffmpgelib/__init__.py
+++ ffmpgelib/__init__.py
@@ -12,10 +12,17 @@ Modules:
 """
 __author__ = 'hernani'
 __version__ = 'ffmpeglib-0.0.1'
+__apply__ = 'console - Mediaplayer with Image PIL'
+__version__ = '1'
 
 try:
     from .ffmplayer import MediaImage
+    from .graphicblock import Graphics
+    from .imageblock import ImageBlock
 except:
     from ffmplayer import MediaImage
+    from graphicblock import Graphics
+    from imageblock import ImageBlock
+
 
 
